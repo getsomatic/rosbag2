c20ded7463890e6ce1632ab18c8082da17df396c
pavlo <pmykalov@gmail.com>
2021-02-19 13:52:17 +0200

    save

---
+++ rosbag2_transport/src/rosbag2_transport/bcr_parser.cpp
@@ -0,0 +1,9 @@
+//
+// Created by pavlo on 2/19/21.
+//
+
+#include "bcr_parser.hpp"
+
+namespace rosbag2_transport {
+
+}
---
+++ rosbag2_transport/src/rosbag2_transport/bcr_parser.hpp
@@ -0,0 +1,19 @@
+//
+// Created by pavlo on 2/19/21.
+//
+
+#ifndef ROSBAG2_TRANSPORT_BCR_PARSER_HPP
+#define ROSBAG2_TRANSPORT_BCR_PARSER_HPP
+
+#include <>
+
+namespace rosbag2_transport {
+    class BcrParser {
+    public:
+        BcrParser();
+    private:
+
+    };
+}
+
+#endif //ROSBAG2_TRANSPORT_BCR_PARSER_HPP
